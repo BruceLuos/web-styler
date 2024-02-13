230cf071d2805f291e346dff37ee20fe2a8248c0
BruceLuo <295448382@qq.com>
2024-02-13 18:29:46 +0800

    feat: login alert

--- src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,23 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { SignIn } from "@clerk/nextjs";
+import { AlertCircle } from "lucide-react";
 import React from "react";
 
 const Page = () => {
-  return <SignIn />;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Alert className=" mb-2 w-[25rem]">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You Can Use The Test Account to Login
+          <p>Account: test@qq.com</p>
+          <p>Password: test</p>
+        </AlertDescription>
+      </Alert>
+      <SignIn />
+    </div>
+  );
 };
 
 export default Page;
