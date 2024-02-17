76d163cc1a46327654bef2576f00c4cf12417259
BruceLuo <295448382@qq.com>
2024-02-17 12:22:09 +0800

    chore: automations init

---
+++ src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
@@ -0,0 +1,10 @@
+import BlurPage from "@/components/global/blur-page";
+import React from "react";
+
+type Props = {};
+
+const Automations = async (props: Props) => {
+  return <BlurPage>Automations will be coming soon </BlurPage>;
+};
+
+export default Automations;
